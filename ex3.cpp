301c8a7abcad079056212b7aa3af024c149d3890
DaiYumi <123912197+DaiYumi@users.noreply.github.com>
2023-01-30 11:17:29 +0800

    Create ex3.cpp

---
+++ ex3.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout<<"Hello World yumidai,your tall is "<<1<<" m "<<68<<" cm "<<endl;
+    
+    printf("Hello World yumidai,your tall is %d m %d cm \n",1,68);
+    printf(" this building is %10d cm \n", 890);
+    printf(" this building is %10d cm \n", 7890);
+    printf(" this building is %10d cm \n", 67890);
+    printf(" this building is %10d cm \n", 567890);
+    printf(" this building is %5d cm \n", 1234567890);
+    printf(" pi = %f \n ", 3.1415926535897932384626433832795);
+     
+    return 0;
+}
